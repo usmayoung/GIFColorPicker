935d8790639d3d8bffcd07e6d32fcea12f36bfdf
Eric Young <ericyoung@gmail.com>
2016-03-21 22:37:47 -0400

    Initial Commit

---
+++ GIFColorPicker/GIFColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  GIFColorPicker.h
+//  GIFColorPicker
+//
+//  Created by Eric Young on 3/21/16.
+//  Copyright © 2016 gifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GIFColorPicker.
+FOUNDATION_EXPORT double GIFColorPickerVersionNumber;
+
+//! Project version string for GIFColorPicker.
+FOUNDATION_EXPORT const unsigned char GIFColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GIFColorPicker/PublicHeader.h>
+
+
